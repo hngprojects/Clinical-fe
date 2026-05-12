981efb6117fff534f9618611cb11ec7f022505ea
idris-umaru <idrisumaru123@gmail.com>
2026-05-12 09:38:51 +0100

    feat(email-templates): add verification failed doctor email template

--- src/components/email-template/index.ts
+++ src/components/email-template/index.ts
@@ -1,7 +1,9 @@
-export { WelcomeUserEmail } from "./WelcomeUserEmail";
-export { WelcomeDoctorEmail } from "./WelcomeDoctorEmail";
-export { PasswordResetUserEmail } from "./PasswordResetUserEmail";
-export { PasswordResetDoctorEmail } from "./PasswordResetDoctorEmail";
-export { VerificationUserEmail } from "./VerificationUserEmail";
-export { VerificationDoctorEmail } from "./VerificationDoctorEmail";
-export { WaitlistConfirmationUserEmail } from "./WaitlistConfirmationUserEmail";
+export { WelcomeUserEmail } from './WelcomeUserEmail';
+export { WelcomeDoctorEmail } from './WelcomeDoctorEmail';
+export { PasswordResetUserEmail } from './PasswordResetUserEmail';
+export { PasswordResetDoctorEmail } from './PasswordResetDoctorEmail';
+export { VerificationUserEmail } from './VerificationUserEmail';
+export { VerificationDoctorEmail } from './VerificationDoctorEmail';
+export { VerificationFailedDoctorEmail } from './VerificationFailedDoctorEmail';
+export { WaitlistConfirmationUserEmail } from './WaitlistConfirmationUserEmail';
+export { DoctorReviewSubmittedUserEmail } from './DoctorReviewSubmittedUserEmail';
